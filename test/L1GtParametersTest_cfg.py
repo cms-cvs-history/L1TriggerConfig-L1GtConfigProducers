7752050fab93bf1cbb546da96ba77445f9060d6d
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2009-03-28 01:41:05 +0000

    Adapted to L1GtParameters with number of bunch crosses per board (DAQ and EVM record). Added BPTX and MHT in parser, fixed overflow treatment.

--- test/L1GtParametersTest_cfg.py
+++ test/L1GtParametersTest_cfg.py
@@ -1,5 +1,3 @@
-# The following comments couldn't be translated into the new config version:
-
 # cfg file to test L1 GT parameters
 
 import FWCore.ParameterSet.Config as cms
@@ -22,3 +20,26 @@ process.load("L1TriggerConfig.L1GtConfigProducers.L1GtParametersConfig_cff")
 # path to be run
 process.p = cms.Path(process.l1GtParametersTest)
 
+# Message Logger
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.debugModules = ['l1GtParametersTest']
+process.MessageLogger.cout = cms.untracked.PSet(
+    threshold=cms.untracked.string('DEBUG'),
+    #threshold = cms.untracked.string('INFO'),
+    #threshold = cms.untracked.string('ERROR'),
+    DEBUG=cms.untracked.PSet(
+        limit=cms.untracked.int32(-1)
+    ),
+    INFO=cms.untracked.PSet(
+        limit=cms.untracked.int32(-1)
+    ),
+    WARNING=cms.untracked.PSet(
+        limit=cms.untracked.int32(-1)
+    ),
+    ERROR=cms.untracked.PSet(
+        limit=cms.untracked.int32(-1)
+    ),
+    default = cms.untracked.PSet( 
+        limit=cms.untracked.int32(-1)  
+    )
+)
